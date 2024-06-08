748f7c222858adab85bc850baf0ae54335d4aade
ddxmu <37870620+ddxmu@users.noreply.github.com>
2024-06-08 21:32:34 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-const routes = {
-  "docker.mydomain.com": "https://registry-1.docker.io",
-  "quay.mydomain.com": "https://quay.io",
-  "gcr.mydomain.com": "https://gcr.io",
-  "k8s-gcr.mydomain.com": "https://k8s.gcr.io",
-  "k8s.mydomain.com": "https://registry.k8s.io",
-  "ghcr.mydomain.com": "https://ghcr.io",
-  "cloudsmith.mydomain.com": "https://docker.cloudsmith.io",
-};
+import { handleRequest } from './handler'
+
+declare global {
+  const HAMMAL_CACHE: KVNamespace
+}
+
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request))
+})
